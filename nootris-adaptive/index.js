f5e6d5f3aa027ba2f6a127c37b897cc3942b0145
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-09-16 14:21:34 +0300

    feat: add function for burger-menu & add styles for header

---
+++ nootris-adaptive/index.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".menu__list");
+const links = document.querySelectorAll(".menu__item");
+
+function slider() {
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        burger.classList.toggle("toggle");
+
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `nav-links-fade .5s ease forwads ${
+                    index / 7 + 0.3
+                }s`;
+            }
+            // console.log(link.style.animation==='');
+        });
+    });
+}
+
+slider();
