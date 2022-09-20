fef302ee6b3cb4da1f0a3bf8d9e2ce82e104e999
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-09-20 12:19:58 +0300

    fix: fix validator warnings

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const navToggle = document.querySelector(".nav-toggle");
-const burgerMenu = document.querySelector(".main-menu");
+const burgerMenu = document.querySelector(".burger-menu");
 const navToggleLabel = document.querySelector(".nav-toggle-label");
-    
+
 navToggle.addEventListener("click", () => {
     burgerMenu.classList.toggle("active");
     navToggleLabel.classList.toggle("active");
 });
-    
+
 burgerMenu.addEventListener("click", (event) => {
-        if(event.target){
-            burgerMenu.classList.remove("active");
-            navToggleLabel.classList.remove("active");
-        }
+    if (event.target) {
+        burgerMenu.classList.remove("active");
+        navToggleLabel.classList.remove("active");
+    }
 });
